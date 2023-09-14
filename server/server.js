77479e3828d6c26ee6ee1b9869709ae5cb094adf
xlazure <lukasz.cielak@gmail.com>
2023-09-14 04:41:06 +0200

    html content on server

--- server/server.js
+++ server/server.js
@@ -13,6 +13,10 @@ app.use(cors());
 
 app.use("/api", routes);
 
+app.get("/", async (req, res, next) => {
+  return res.status(200).send("<h1>EXE TASK API</h1>");
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
